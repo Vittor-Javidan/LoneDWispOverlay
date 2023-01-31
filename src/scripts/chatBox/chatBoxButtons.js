65128f2152bdff2d20434f14728645c670b60f9b
Vittor-Javidan <vittor.javidan.t@gmail.com>
2023-01-31 13:10:53 -0300

    UPDATE: Now chatbox has a button to hide and show

---
+++ src/scripts/chatBox/chatBoxButtons.js
@@ -0,0 +1,14 @@
+const chatBoxHideButton = document.getElementById('chatbox-hide-button');
+const chatContainer = document.getElementById('chatbox')
+
+chatBoxHideButton.addEventListener('click', () => {
+
+    chatContainer.classList.toggle('no-chat')
+    
+
+    if(chatBoxHideButton.innerText === 'Hide') {
+        chatBoxHideButton.innerText = 'Show'
+        return
+    }
+    chatBoxHideButton.innerText = 'Hide'
+});
